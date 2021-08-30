0fbac9dfbe880821b946400e37e40a2129244263
Julian <julian.hecker@railinc.com>
2021-08-30 15:39:45 -0400

    add student model

--- src/main/java/com/hecker/learningspringboot/LearningSpringbootApplication.java
+++ src/main/java/com/hecker/learningspringboot/LearningSpringbootApplication.java
@@ -1,13 +1,27 @@
 package com.hecker.learningspringboot;
 
+import com.hecker.learningspringboot.person.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class LearningSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringbootApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public List<Student> hello() {
+		return List.of(
+				new Student("boolean", "boolean@gmail.com", LocalDate.of(1999, Month.DECEMBER, 9), 21)
+		);
+	}
 }
---
+++ src/main/java/com/hecker/learningspringboot/person/Student.java
@@ -0,0 +1,61 @@
+package com.hecker.learningspringboot.person;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
