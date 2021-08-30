544440082a77dd3b1557455b16f5fa2a9a097f12
Julian <julian.hecker@railinc.com>
2021-08-30 13:46:53 -0400

    initial commit

---
+++ src/test/java/com/hecker/learningspringboot/LearningSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hecker.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
